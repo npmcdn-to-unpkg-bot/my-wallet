b04a0aa45b496d1ad6899cd84a02902426f615a8
Maxmiliano Andriani <max.andriani@gmail.com>
2016-05-11 20:02:44 -0300

    some fixes on List model

--- lists/listModel.js
+++ lists/listModel.js
@@ -36,7 +36,7 @@ List.prototype.delete = function( next ){
     try{
         // Load DB
         var db = require(global.pathTo('/db/dbModel.js'));
-        var clearTransactionsQuery, removeListsQuery;
+        var clearTransactionsQuery, removeListQuery;
         var listId = this.list_id;
         
         clearTransactionsQuery = 
@@ -109,6 +109,35 @@ List.prototype.removeTransaction = function( transactionId, next ){
 };
 
 /**
+ * List.addTransaction()
+ * Add an transaction into the current list
+ * 
+ * @param {number} transactionId The transaction reference
+ * @param {function} next Callback function
+ * @returns {undefined}
+ */
+List.prototype.addTransaction = function( transactionId, next ){
+    var db = require(global.pathTo('/db/dbModel.js'));
+    var query = 
+        'Update '+
+            'transactions as t '+
+        'Set '+
+            't.list_id = ? '+
+        'Where '+
+            't.id = ? '+
+        'Limit 1;';
+    var values = [ this.list_id, transactionId ];
+    
+    db.query( query, values, function(err){
+        if (err){
+            next(new Error('ERROR_LISTS_UNNABLE_TO_ADD_TRANSACTION'));
+        } else {
+            next(false);
+        }
+    });
+};
+
+/**
  * List.save()
  * Save the current metadata in database.
  * 
@@ -124,7 +153,7 @@ List.prototype.save = function( next ){
                 'Set '+
                    'name = \'?\' '+
                 'Where '+
-                    'users.id = \'?\' '+
+                    'lists.id = \'?\' '+
                 'Limit 1;';
         db.query( query, [_self.list_name, _self.list_id], function(err){
             if (err){
@@ -140,235 +169,122 @@ List.prototype.save = function( next ){
 };
 
 /**
- * User.passwd
- * Change the user's Password
+ * Lists
  * 
- * @param {string} passwd A string with user's password. Do not encrypt this string.
- * @param {function} next A callback function that will be triggered after change the password.
- * @returns {undefined}
+ * This object will manage the plural List behaviour
  */
-User.prototype.changePass = function( passwd, next ){
-    try {
-        var db = require(global.pathTo('/db/dbModel.js'));
-        var sha1 = require('crypto-js/hmac-sha1');
-        var passwd_hash;
-        
-        passwd_hash = sha1.encrypt(passwd, global.config.SESSION_SECRET);
-        
-        var query = 'Update users Set password=\'?\' Limit 1;';
-        
-        db.query( query, [passwd_hash], next );
-    } catch(err) {
-        next(err);
-    }
-};
+var Lists = function(){};
 
 /**
- * User.addAccount
- * Add an user's account.
- * 
- * @param {Account} account Account abstract object
- * {
- *     email: {string},
- *     type: {number},
- *     secret: {string},
- *     token: {string}
- * }
- * @param {function} next A callback function that will be triggered after add an new Account and attach to an user
- * @returns {undefined}
- */
-User.prototype.addAccount = function( account, next ){
-    try{
-        var db = require(global.pathTo('/db/dbModel.js'));
-        var query = 'Insert Into auth (auth_type_id, user_id, email,';
-        var values = [];
-        
-        values.push(account.type);
-        values.push(this.user_id);
-        values.push(account.email);
-        
-        if (typeof account.secret !== 'undefined'){
-            query += ' auth_key,';
-            values.push(account.secret);
-        }
-        if (typeof account.token !== 'undefined'){
-            query += ' auth_token,';
-            values.push(account.token);
-        }
-        if (typeof account.is_primary !== 'undefined'){
-            query += ' is_primary,';
-            values.push(account.is_primary);
-        }
-        
-        query = query.slice(0,-1);
-        query += ") Values (?, ?, '?',";
-        
-        if (typeof account.secret !== 'undefined'){
-            query += " '?',";
-        }
-        if (typeof account.token !== 'undefined'){
-            query += " '?',";
-        }
-        if (typeof account.is_primary !== 'undefined'){
-            query += " '?',";
-        }
-        
-        query = query.slice(0,-1);
-        query += ');';
-        
-        db.query( query, values, next );
-    } catch (err) {
-        next(err);
-    }
-};
-
-/**
- * User.setPrimary
- * Set an Account to primary account
- * 
- * @param {string} email The Account email
- * @param {function} next The callback function that will be triggered after set an account to primary
- * @returns {undefined}
- */
-User.prototype.setPrimary = function( email, next ){
-    try{
-        var db = require(global.pathTo('/db/dbModel.js'));
-        var userId = this.user_id;
-        var queryReset = 'Update auth Set auth.primary = 0 Where auth.primary = 1 And auth.user_id = \'?\' Limit 1;';
-        var queryUpdate = 'Update auth Set auth.primary = 1 Where email=\'?\' And auth.user_id = ? Limit 1;';
-        
-        db.query( queryReset, [userId], function(err){
-            if (err){
-                next(err);
-            }
-            
-            // Reset is OK
-            db.query( queryUpdate, [email, userId], next );
-        });
-        
-    } catch(err) {
-        next(err);
-    }
-};
-
-/**
- * Users
- * 
- * This object will manage the plural User behaviour
- */
-var Users = function(){};
-
-/**
- * Users.find()
- * Search in the database for an user, that factory an User object to each result
+ * Lists.find()
+ * Search in the database for an list, that factory an User object to each result
  * 
  * @param {object} find The search param object
  * {
  *      search: {string}, The search query
- *      fields: {array}, Fields that search will perform (default: ['user_name', 'user_email'],
+ *      user_id: {number}, The user reference
  *      page: {number}, The pagination reference
  * }
  * @param {function} next A callback function
  * @returns {undefined}
  */
-Users.prototype.find = function( find, next ){
+Lists.prototype.find = function( find, next ){
     
     var db = require(global.pathTo('/db/dbModel.js'));
     var query;
     var values = [];
-    
-    if (typeof find.search === 'undefined'){
-        find.search = null;
-    }
-    
-    if (typeof find.fields === 'undefined'){
-        find.fileds = [];
-    }
-    
-    if (typeof find.page === 'undefined'){
-        find.page = 0;
-    }
-    
-    if (typeof next !== 'function'){
-        next = function(){};
-    }
-    
-    // Start tu build query
-    query = 'Select '+
-                'u.id as user_id,'+
-                'u.name as user_name,'+
-                'a.email as user_email '+
+        
+    try{
+        if (typeof find.user_id === 'undefined'){
+            throw new Error('ERROR_LISTS_INVALID_USER_ID');
+        }
+        
+        if (typeof find.search === 'undefined'){
+            find.search = null;
+        }
+        
+        if (typeof find.page === 'undefined'){
+            find.page = 0;
+        }
+        
+        // Start tu build query
+        query = 
+            'Select '+
+                'l.id as list_id,'+
+                'l.user_id as user_id,'+
+                'l.name as list_name '+
             'From '+
-                'users as u '+
-            'Left Join '+
-                'auth as a On a.user_id And a.is_primary = 1 ';
+                'lists as l '+
+            'Where '+
+                'l.user_id = ? ';
+
+        values.push(find.user_id);
+        
+        if (find.search){
+            // Find by key
+            var nameString = find.search.replace(/\s/g, '%');
+            query += "And l.name Like '%?%' ";
+            values.push(nameString);
+        } else {
+            // List all
+            // Nothing to do
+        }
+        
+        // Sort and order
+        query += ' Order By l.name Asc ';
     
-    if (find.search){
-        // Find by key
-        var nameString = find.search.replace(/\s/g, '%');
-        var emailString = find.search;
-        query += 'Where ';
-        query += " u.name Like '%?%'";
-        values.push(nameString);
-        query += " a.email = '?'";
-        values.push(emailString);
-    } else {
-        // List all
-        // Nothing to do
+        // Pagination
+        query += ' Limit ?, 9';
+        values.push((find.page * 9));
+    
+        // End query
+        query += ';';
+    
+        // Do the magic
+        db.query( query, values, function(err, data){
+            if (err){
+                next(err);
+                return;
+            }
+
+            var listList = [];
+            for(var x in data){
+                listList.push(new List(data[x]));
+            }
+
+            next(false, listList);
+        });
+    } catch(err) {
+        next(err);
     }
-    
-    // Sort and order
-    query += ' Order By u.name Asc';
-    
-    // Pagination
-    query += ' Limit ?, 9';
-    values.push((find.page * 9));
-    
-    // End query
-    query += ';';
-    
-    // Do the magic
-    db.query( query, values, function(err, data, fields){
-        if (err){
-            next(err);
-            return;
-        }
-        
-        var userList = [];
-        for(var x in data){
-            userList.push(new User(data[x]));
-        }
-        
-        next(false, userList);
-    });
 };
 
 /**
- * Users.getUserById()
- * Get user's info from database and factory an User Object
+ * Lists.getListById()
+ * Get lists's info from database and factory an List Object
  * 
- * @param {number} user_id The user ID
+ * @param {number} user_id The user reference
+ * @param {number} list_id The List reference
  * @param {function} next Callback function
  * @returns {undefined}
  */
-Users.prototype.getUserById = function( user_id, next ){
+Lists.prototype.getListById = function( user_id, list_id, next ){
     var db = require(global.pathTo('/db/dbModel.js'));
     var query;
     
     query = 'Select '+
-                'u.id as user_id,'+
-                'u.name as user_name,'+
-                'a.email as user_email '+
+                'l.id as list_id,'+
+                'l.name as list_name,'+
+                'l.user_id as user_id '+
             'From '+
-                'users as u '+
-            'Left Join '+
-                'auth as a On a.user_id And a.is_primary = 1 '+
+                'lists as l '+
             'Where '+
-                "u.id = ? "+
+                "l.id = ? And "+
+                "l.user_id = ?"+
             'Limit 1;';
     
     // Do the magic
-    db.query( query, [user_id], function(err, user){
+    db.query( query, [list_id, user_id], function(err, user){
         if (err){
             next(err);
             return;
@@ -377,121 +293,76 @@ Users.prototype.getUserById = function( user_id, next ){
         if (user.length == 1){
             next(false, new User(user[0]));
         } else {
-            next( new Error('404: ERROR_USER_NOT_FOUND') );
+            next( new Error('404: ERROR_LIST_NOT_FOUND') );
         }
     });
 };
 
 /**
- * Users.getUserByEmail()
- * Get user's info from database and factory an User Object
+ * Lists.getTransactions
+ * Get all transactions with this list
+ * Alias to transactionModel.getTransactionsByList(user_id, list_id, next);
  * 
- * @param {string} user_email The user's email
- * @param {function} next Callback function
+ * @param {number} user_id The user reference
+ * @param {number} list_id The lisst reference
+ * @param {type} next The callback function
  * @returns {undefined}
  */
-Users.prototype.getUserByEmail = function( user_email, next ){
-    this.find({
-        search: user_email,
-        fields: ['email']
-    }, next);
+Lists.prototype.getTransactions = function(user_id, list_id, next){
+    var transactionModel = require(global.pathTo('/transactions/transactionModel.js'));
+    transactionModel.getTransactionsByList(user_id, list_id, next);
 };
 
 /**
- * Users.insertUser()
- * Insert a new user in the database
+ * Lists.insertList()
+ * Insert a new list in the database
  * 
- * @param {object} user_data An User Object or an abstract user object
+ * @param {object} list_data An List Object or an abstract list object
  * {
- *     user_name: {string}
- *     user_email: {string}
- *     password: {string}
+ *     user_id: {number}
+ *     list_name: {string}
  * }
  * @param {function} next Callback function
  * @returns {undefined}
  */
-Users.prototype.insertUser = function( user_data, next ){
+Lists.prototype.insertList = function( data, next ){
     var db = require(global.pathTo('/db/dbModel.js'));
-    var checkQuery;
-    var checkValues = [];
     var insertQuery;
     var insertValues = [];
     
-    checkQuery = 'Select '+
-                    'u.id as user_id, '+
-                    'u.name as user_name, '+
-                    'a.email as user_email '+
-                 'From '+
-                    'auth as a '+
-                 'Left Join '+
-                    'users as u On u.id = a.user_id '+
-                 'Where '+
-                    'a.email = \'?\' Limit 1;';
-    checkValues.push(user_data.user_email);
-    
     insertQuery = 'Insert Into '+
-                    'users '+
-                        '(name, password) '+
+                    'lists '+
+                        '(name, user_id) '+
                     'Values '+
-                        "('?', '?');";
-    insertValues.push(user_data.user_name);
-    insertValues.push(user_data.password);
+                        "('?', ?);";
+    insertValues.push(data.list_name);
+    insertValues.push(data.user_id);
     
-    var afterCheck = function(err, data, fields){
-        if (err){
-            next(err);
-            return;
-        }
-        
-        if (data.length == 0){
-            // Insert User
-            db.query( insertQuery, afterInsert );
-        } else {
-            next(new Error('ERROR_USER_ALREADY_EXISTS'));
-        }
-    };
-    
-    var afterInsert = function(err, data, fields){
+    db.query( insertQuery, insertValues, function(err, db_data, fields){
         if (err){
             next(err);
         }
         
-        user_data.user_id = data.insertId;
+        data.list_id = db_data.insertId;
         
-        var user = new User(user_data);
-        var account = {
-            email: user_data.user_email,
-            type: 1,
-            is_primary: 1
-        };
-        
-        user.addAccount( account , function(err){
-            if (err){
-                next(err);
-            }
-            
-            next(false, user);
-        });
-    };
-    
-    // Check
-    db.query( checkQuery, afterCheck);
+        next(false, new List(data));
+    });
     
 };
 
 /**
- * Users.factory
- * Factory an user object with an user abstract object data
- * @param {object} user_data User abstract object data
+ * List.factory
+ * Factory an list object with an list abstract object data
+ * @param {object} list_data List abstract object data
  * {
- *     user_id: {number},
- *     user_name: {string},
- *     user_email: {string}
+ *     list_id: {number},
+ *     list_name: {string},
+ *     user_id: {number}
  * }
- * @returns {User}
+ * @returns {List}
  */
-Users.prototype.factory = function( user_data ){
-    return new User( user_data );
+Lists.prototype.factory = function( list_data ){
+    return new List( list_data );
 };
 
-module.exports = new Users();
+module.exports = new Lists();
